d0ee2eef5525a9369bdbceec67a63ce74bccd3cd
Jungmin Moon <jmmoon96@gmail.com>
2024-05-05 16:50:34 -0400

    Redo of how the class file should be structured.

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,36 @@
 public class Player {
 }
+
+/*
+bat
+beetle
+mushroom
+plant
+snake
+treant
+wolf
+worm
+slime
+ghost
+oni
+ogre
+dragon
+griffin
+flying devil
+evil wizard
+undead knight
+spider
+scorpion
+mimic
+stone golem
+jack o lantern
+skeleton
+elder dragons
+elder treants
+jellyfish
+angler fish
+knight
+
+
+
+ */
